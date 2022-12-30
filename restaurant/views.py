940e25a2929cf35eccd7c5f47962169885c60fb5
Kristiyana Nikolova <57190703+Kristiyana00@users.noreply.github.com>
2022-12-30 12:43:53 +0200

    Add files via upload

--- restaurant/views.py
+++ restaurant/views.py
@@ -15,8 +15,11 @@ class Dashboard(LoginRequiredMixin, UserPassesTestMixin, View):
 
         current_user = request.user
 
+        restaurant_name = Restaurant.objects.get(owner=current_user).name
+        restaurant_pk = Restaurant.objects.get(owner=current_user).pk
+
         #Извличаме от базата всички поръчки, които са направени днес.
-        orders = OrderModel.objects.filter(created_on__year=today.year, created_on__month=today.month, created_on__day=today.day)
+        orders = OrderModel.objects.filter(created_on__year=today.year, created_on__month=today.month, created_on__day=today.day).filter(restaurant_id=restaurant_pk)
         
         unshipped_orders = []
         shipped_orders = []
@@ -33,6 +36,7 @@ class Dashboard(LoginRequiredMixin, UserPassesTestMixin, View):
 
         context = {
             'current_user': current_user,
+            'restaurant': restaurant_name,
             'shipped_orders': shipped_orders,
             'unshipped_orders': unshipped_orders,
             'total_revenue': total_revenue,
@@ -51,9 +55,12 @@ class OrderDetails(LoginRequiredMixin, UserPassesTestMixin, View):
         order = OrderModel.objects.get(pk=pk)
 
         current_user = request.user
+
+        restaurant_name = Restaurant.objects.get(owner=current_user).name
         
         context = {
             'current_user': current_user,
+            'restaurant': restaurant_name,
             'order': order
         }
 
@@ -62,12 +69,15 @@ class OrderDetails(LoginRequiredMixin, UserPassesTestMixin, View):
     #Ъпдейтваме базата данни, че поръчката е изпратена, след натискане на бутона "Поръчката е изпратена."
     def post(self, request, pk, *args, **kwargs):
         current_user = request.user
+        restaurant_name = Restaurant.objects.get(owner=current_user).name
+
         order = OrderModel.objects.get(pk=pk)
         order.is_shipped = True
         order.save()
 
         context = {
             'current_user': current_user,
+            'restaurant': restaurant_name,
             'order': order
         }
 
@@ -80,10 +90,13 @@ class OrderDetails(LoginRequiredMixin, UserPassesTestMixin, View):
 class RestaurantMenu(LoginRequiredMixin, UserPassesTestMixin, View):
     def get(self, request, *args, **kwargs):
         current_user = request.user
+        restaurant_name = Restaurant.objects.get(owner=current_user).name
+
         menu_items = Menu.objects.get(restaurant__owner=current_user.id).menu_items
 
         context = {
             'current_user': current_user, #Подаваме го заради навигацията.
+            'restaurant': restaurant_name,
             'menu_items': menu_items
         }
 
